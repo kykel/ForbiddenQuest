f65da60f3af7c31ba66542224b8b2a5d5b27951b
kykel <kykel09@gmail.com>
2025-04-01 14:57:27 -0400

    updated py2 to py3 and updated print statements. added main execution

--- shops.py
+++ shops.py
@@ -3,7 +3,8 @@
 #The Blacksmith's Armory
 #The Tamer's Den
 import config
-import questcontrolfile
+#import questcontrolfile
+
 config.clear()
 
 
@@ -19,35 +20,29 @@ class Shop():
 		self.merchandise = merchandise
 
 	def clerk(self):
-		choice = raw_input("1. Buy\n2. Sell\n")
+		choice = input("1. Buy\n2. Sell\n")
 		if choice == '1':
 			self.buy()
 		else:
 			pass
 
 	def buy(self):
-		print "Excellent! I have some wonderful merchandise to show you today."
+		print("Excellent! I have some wonderful merchandise to show you today.")
 		for i,c in zip(self.count, self.merchandise):
-			print i,c
+			print(i,c)
 		#self.present()
 
 	def sell(self):
 		pass
-		#print inventory
 
 	def present(self):
-		choice = raw_input("")
+		choice = input("")
 		questcontrolfile.funccall(choice, "shortsword", "longsword", "spear", "bow", "shield")
-		
-
-
-
-
 
 
 class Armory(Shop):
 
-	merchandise = ["Shortsword", "Longsword", "Spear", "Bow", "Shield"]
+	merchandise = ["Shortsword", "Longsword", "Spear", "Bow", "Shield","Exit"]
 	name = "The Blacksmith's Armory"
 	description = "\nYou enter the armory to find yourself in a surprisingly nice room. A door leads to the back where the smell of iron and soot seeps in from. Obviously, the armory itself is in the back. The clerk at the front desk is a bear of a man and looks battle worn and hard, decorated more by scars than freckles.\n"
 	artform = "Weapons"
@@ -56,17 +51,17 @@ class Armory(Shop):
 		pass
 	
 	def enter(self):
-		print self.description
-		print "'Welcome to my armory friend! All weapons expertly crafted by yours truly!' He bellows as you look around the store.\n'Looking for anything in particular? All my blades are freshly sharpened!'\n"
+		print(self.description)
+		print("'Welcome to my armory friend! All weapons expertly crafted by yours truly!' He bellows as you look around the store.\n'Looking for anything in particular? All my blades are freshly sharpened!'\n")
 		self.clerk()
 
 
 
 class Hollow(Shop):
 
-	merchandise = ["Fire", "Ice", "Wind", "Heal", "Revive"]
+	merchandise = ["Fire", "Ice", "Wind", "Heal", "Revive","Exit"]
 	name = "The Wizard's Hollow"
-	description = "\nYou enter the Wizard's Hollow and immediatly begin to wonder if being a wizard is the best career path for you. The place is unnervingly dark and once you're eyes finally adjust you see the walls are lined with shelves and jarred specimens.\nA rotting smell draws your eyes to one corner of the store where a floating bat stares at you through pale eyes in a tank...\n"
+	description = "\nYou enter the Wizard's Hollow and immediately wonder if being a wizard is the best career path for you. The place is unnervingly dark and once you're eyes finally adjust you see the walls are lined with shelves and jarred specimens.\nA rotting smell draws your eyes to one corner of the store where a floating bat stares at you through pale eyes in a tank...\n"
 	artform = "magic"
 		
 	
@@ -74,8 +69,8 @@ class Hollow(Shop):
 		pass
 
 	def enter(self): 
-		print self.description
-		print "\nA creepy old hag of a lady approaches from behind, more silent than a mouse.\n'Welcome to", self.name + "!' You jump when she speaks to you, surprised by how close she got without you seeing her.\n\n'May I assist you?' she asks.\n You find yourself staring at her glass eye..."
+		print(self.description)
+		print("\nA creepy old hag of a lady approaches from behind, more silent than a mouse.\n'Welcome to", self.name + "!' You jump when she speaks to you, surprised by how close she got without you seeing her.\n\n'May I assist you?' she asks.\n You find yourself staring at her glass eye...")
 		self.clerk()
 
 	
@@ -83,23 +78,23 @@ class Hollow(Shop):
 
 class Pets(Shop):
 
-	merchandise = ["Hawk", "Rattlesnake", "Monitor Lizard", "Pony", "Goat", "Hunting Dog"]
+	merchandise = ["Hawk", "Rattlesnake", "Monitor Lizard", "Pony", "Goat", "Hunting Dog","Exit"]
 	name = "The Tamer's den"
-	description = "\nThe first thing you notice as you walk into the Tamer's Den, is the smell of urin and an assortment of wet animal smells, mostly dog.\nThe place is huge, more a barn than store, and just past the front desk it stretches back into pens and stables. A large fence lines the ceiling and comes down the walls into atriaries for the magnificent assortment of birds the owner sports.\nBehind the desk a thin pale man with bright blue eyes stares at you eagerly.\n"
+	description = "\nSmells accost your nose as you enter the Tamer's Den, the smell of urin and an assortment of wet animal smells, mostly dog.\nThe place is huge, more a barn than store, and just past the front desk it stretches back into pens and stables. A large fence lines the ceiling and comes down the walls into atriaries for the magnificent assortment of birds the owner sports.\nBehind the desk a thin pale man with bright blue eyes stares at you eagerly.\n"
 	artform = "Companions"
 
 	def __init__(self):
 		pass
 	
 	def enter(self):
-		print self.description
-		print "'May I help you? Normally, we have quite the wonderful selection but I'm afraid most of my animals are actually purchased already. Supply and demand you know...\n We do however, have some variety left if you'd like to look.'\n"
+		print(self.description)
+		print("'May I help you? Normally, we have quite the wonderful selection but I'm afraid most of my animals are actually purchased already. Supply and demand you know...\n We do however, have some variety left if you'd like to look.'\n")
 		self.clerk()
 
 
 
 def decision():
-	choice = raw_input("Where do you want to go?\n1. Wizard's Hollow\n2. Blacksmith's Armory\n3. Tamer's Den\n")
+	choice = input("Where do you want to go?\n1. Wizard's Hollow\n2. Blacksmith's Armory\n3. Tamer's Den\n")
 	if choice == '1':
 		WH.enter()
 	elif choice == '2':
@@ -107,13 +102,7 @@ def decision():
 	elif choice == '3':
 		TD.enter()
 	else:
-		print "Incorrect choice\n"
-
-
-WH = Hollow()
-BA = Armory()
-TD = Pets()
-decision()
+		print("Incorrect choice\n")
 
 #class shop(self):
 
@@ -130,4 +119,8 @@ decision()
 #shoptoenter.enter()
 
 
-
+if __name__ == "__main__":
+	WH = Hollow()
+	BA = Armory()
+	TD = Pets()
+	decision()
