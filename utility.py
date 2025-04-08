2f60cd75c357cfac2831f3c14cc4010052156675
kykel <kykel09@gmail.com>
2025-04-07 22:43:47 -0400

    Reformatted universal. Added hidden menu. Removed self references.

--- utility.py
+++ utility.py
@@ -2,18 +2,25 @@
 #config file for global variables and stuffs
 import os
 import time
+from contextlib import suppress
 
 class Utility():
 
-    # Pause until player input
+    # Class variables
+    level = 0
+    health = 10
+    battles = 0
+    name = "Chosen-One"
+    playerdmg = [1,4]    # Pause until player input
     def pause():
         return input("\nPress Enter to Continue.\n")
 
     # Clear/refresh the screen
     def clear():
         return os.system('cls' if os.name == 'nt' else 'clear')
+
     def validateyesorno():
-        import mainfile
+        #import mainfile
         i = 1
         while i < 2:
             choice = input('Yes or no?\n')
@@ -32,7 +39,7 @@ class Utility():
                print("Invalid input. ")
                exit()
 
-    def universal_menu(self, menuitems: list, header: str = "") -> str | None:
+    def universal_menu(menuitems: list, header: str = "") -> str | None:
         """ Generate a generic Menu based on *menuitems*. """
         while True:
 
@@ -41,7 +48,10 @@ class Utility():
 
             #Skyler's version
             for k, v in enumerate(menuitems, start=1):
-                print(f"{k:d}. {str(v).title()}")
+                if v == "blank":
+                    pass
+                else:
+                    print(f"{k:d}. {str(v).title()}")
             answer = input("Select an Option (by number or name)\n> ").title()
             with suppress(ValueError, IndexError):
                 #return menuitems[int(answer) - 1].title()  # because 0-indexing - used for returning a string
